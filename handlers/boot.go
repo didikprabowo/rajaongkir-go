647ded042d5a997cf950e7e27fd69e10da9ac399
Didik Prabowo <didik.prabowo@digitalsiber.id>
2019-04-15 15:04:34 +0700

    env with os

--- handlers/boot.go
+++ handlers/boot.go
@@ -1,8 +1,19 @@
 package handlers
 
-//config URL and KEY
-const TYPERJ string = "starter"
-const URLProvince string = "https://api.rajaongkir.com/" + TYPERJ + "/province"
-const URLCity string = "https://api.rajaongkir.com/" + TYPERJ + "/city"
-const URLCost string = "https://api.rajaongkir.com/" + TYPERJ + "/cost"
-const key string = "6141096f7c8fed1a3226cc03ac3dab0b"
+import (
+	"os"
+)
+
+// OS
+var TYPERJ string = os.Getenv("TYPERJ")
+var URLProvince string = os.Getenv("URLProvince")
+var URLCity string = os.Getenv("URLCity")
+var URLCost string = os.Getenv("URLCost")
+var key string = os.Getenv("key")
+
+//HARDCODE
+// const TYPERJ string = "starter"
+// const URLProvince string = "https://api.rajaongkir.com/" + TYPERJ + "/province"
+// const URLCity string = "https://api.rajaongkir.com/" + TYPERJ + "/city"
+// const URLCost string = "https://api.rajaongkir.com/" + TYPERJ + "/cost"
+// const key string = "6141096f7c8fed1a3226cc03ac3dab0b"
